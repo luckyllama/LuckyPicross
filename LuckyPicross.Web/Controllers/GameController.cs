c8c057ed091efee8608a3bbabec0eaa51e614609
frank hadder <luckyllama@gmail.com>
2012-01-15 00:51:16 -0600

    initial setup and commit

---
+++ LuckyPicross.Web/Controllers/GameController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AttributeRouting;
+
+namespace LuckyPicross.Web.Controllers
+{
+    public class GameController : Controller
+    {
+        [Route("editor")]
+        public ActionResult Editor()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ LuckyPicross.Web/content/scripts/picross.js
@@ -0,0 +1,36 @@
+﻿
+
+
+var Picross = (function ($) {
+
+    var defaults = {
+        editorMode: false,
+        boardWidth: 10,
+        boardHeight: 10
+    };
+
+    var picross = Class.extend({
+        init: function (canvasSelector, options) {
+            this.options = $.extend({}, defaults, options);
+
+            this.gameState = new gameState(options.boardWidth, options.boardHeight, );
+
+            var canvas = $(canvasSelector).get(0);
+            this.context = canvas.getContext("2d");
+            render();
+        },
+        render: function () {
+
+        }
+    });
+
+    var gameState = Class.extend({
+        init: function (width, height, board) {
+            this.width = width;
+            this.height = height;
+            this.board = board;
+        }
+    });
+
+    return picross;
+})(jQuery);
