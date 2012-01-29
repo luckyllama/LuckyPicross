6ba030d33b590e6c1353489f9c87aded3415f3c1
luckyllama <luckyllama@gmail.com>
2012-01-29 05:13:05 +0000

    got everything working that was

--- src/app.js
+++ src/app.js
@@ -3,8 +3,8 @@
  * Module dependencies.
  */
 
-var express = require('express')
-  , routes = require('./routes')
+var express = require('express'),
+    routes = require('./routes');
 
 var app = module.exports = express.createServer();
 
@@ -13,6 +13,7 @@ var app = module.exports = express.createServer();
 app.configure(function(){
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
+  app.set('view options', { layout: false });
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
@@ -30,6 +31,7 @@ app.configure('production', function(){
 // Routes
 
 app.get('/', routes.index);
+app.get('/editor', routes.editor);
 
-app.listen(3000);
+app.listen(process.env.PORT);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
--- src/routes/index.js
+++ src/routes/index.js
@@ -4,5 +4,9 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' })
+  res.render('index', { title: 'Express' });
+};
+
+exports.editor = function(req, res) {
+  res.render('game/editor', { title: 'Puzzle Creator' });  
 };
