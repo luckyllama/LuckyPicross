bb11986ff20bd65fd276bf3bd9faf37af5585031
frank hadder <luckyllama@gmail.com>
2012-01-29 22:48:44 -0600

    added logic to create hints

--- public/js/picross.js
+++ public/js/picross.js
@@ -1,7 +1,5 @@
-﻿///<reference path="lib/jquery-1.7.1.js"/>
-///<reference path="base.js"/>
-
-var Picross = (function ($) {
+﻿
+window.Picross = (function ($) {
 
     var defaults = {
         editorMode: false,
@@ -16,18 +14,25 @@ var Picross = (function ($) {
 
             this.gameState = new gameState(this.options.width, this.options.height, "");
 
-            this.$game = $(gameSelector);
+            var $game = $(gameSelector);
+            this.gameArea = {
+                $game: $(gameSelector),
+                $board: $(".board", $game),
+                $topHints: $(".top.hints", $game),
+                $sideHints: $(".side.hints", $game)
+            };
             this.render();
+            this.updateHints();
             this.bindEvents();
         },
         render: function () {
-            this.$game.addClass("size-" + this.options.width + "-" + this.options.height);
-            $(".board", this.$game).append(createBoardTable(this.options.height, this.options.width));
-            $(".top.hints", this.$game).append(createTopHintsTable(this.options.width));
-            $(".side.hints", this.$game).append(createSideHintsTable(this.options.height));
+            this.gameArea.$game.addClass("size-" + this.options.width + "-" + this.options.height);
+            this.gameArea.$board.append(createBoardTable(this.options.height, this.options.width));
+            this.gameArea.$topHints.append(createTopHintsTable(this.options.width));
+            this.gameArea.$sideHints.append(createSideHintsTable(this.options.height));
         },
         bindEvents: function () {
-            this.$game.on("hover.picross", "td", function () {
+            this.gameArea.$game.on("hover.picross", "td", function () {
                 var data = $(this).data();
                 if (data && typeof data.col !== "undefined") {
                     $(".col-" + data.col, this.$game).toggleClass("hover");
@@ -37,7 +42,32 @@ var Picross = (function ($) {
                 }
             });
         },
-
+        updateHints: function () {
+            var self = this;
+            var calculateHints = function ($hintsArea, align) {
+                $('td', $hintsArea).each(function () {
+                    var $hintArea = $(this);
+                    var alignIndex = $hintArea.data(align);
+                    var count = 0;
+                    $('td.' + align + '-' + alignIndex, self.gameArea.$board).each(function () {
+                        if($(this).is('.filled')) {
+                            count++;
+                        } else {
+                            if (count) {
+                                $('<span>').text(count).appendTo($hintArea);
+                                count = 0;
+                            }
+                        }
+                    });
+                    if (!$hintArea.find("span").length) {
+                        console.log($hintArea);
+                        $('<span>').text(count).appendTo($hintArea);
+                    }
+                });
+            };
+            calculateHints(this.gameArea.$topHints, 'col');
+            calculateHints(this.gameArea.$sideHints, 'row');
+        },
         log: function () {
             if (this.options.debugMode) {
                 try {
